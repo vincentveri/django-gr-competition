f0aaca2b1c724bc20e4d1776659b0a845d34abf2
Vincent Veri <vincent.veri@gmail.com>
2025-11-21 00:22:46 +0100

    Aggiornata la configurazione delle URL per includere 'anagrafiche.urls' nella root del progetto

--- core/urls.py
+++ core/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('anagrafiche/', include('anagrafiche.urls')),
+    path('', include('anagrafiche.urls')),
 ]
