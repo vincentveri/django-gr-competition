02a2255aea750588c6515a61401f1ea2e8af1585
Vincent <vincent.veri@gmail.com>
2025-11-20 16:29:39 +0100

    Modifica il contesto del rendering nella vista lista_societa per utilizzare 'societa_list' invece di 'societa'

--- anagrafiche/views.py
+++ anagrafiche/views.py
@@ -4,7 +4,7 @@ from .models import Societa
 
 def lista_societa(request):
     societa = Societa.objects.all()
-    return render(request, 'anagrafiche/lista_societa.html', {'societa': societa})
+    return render(request, 'anagrafiche/lista_societa.html', {'societa_list': societa})
 
 
 def crea_societa(request):
