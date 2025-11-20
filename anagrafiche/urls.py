f0bb519a8b202095143095373e5df8c2519c3b57
Vincent <vincent.veri@gmail.com>
2025-11-20 16:19:27 +0100

    Aggiunta della vista per la creazione di nuove società e aggiornamento delle URL. Ristrutturazione dei file CSS e JS, con nuovi percorsi per i file statici.

--- anagrafiche/urls.py
+++ anagrafiche/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('societa/', views.lista_societa, name='lista_societa'),
+    path('societa/nuova/', views.crea_societa, name='crea_societa'),
 ]
--- anagrafiche/views.py
+++ anagrafiche/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Societa
 
 def lista_societa(request):
     societa = Societa.objects.all()
     return render(request, 'anagrafiche/lista_societa.html', {'societa': societa})
+
+
+def crea_societa(request):
+    if request.method == 'POST':
+        nome = request.POST.get('nome')
+        codice_fiscale = request.POST.get('codice_fiscale')
+        nuova_societa = Societa(nome=nome, codice_fiscale=codice_fiscale)
+        nuova_societa.save()
+        return redirect('lista_societa')
+    return render(request, 'anagrafiche/crea_societa.html')
