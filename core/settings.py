164f591481346e57ad99660232a7a5d03cd418c5
Vincent <vincent.veri@gmail.com>
2025-11-21 13:03:11 +0100

    Aggiunta dell'app 'competizioni' con configurazione iniziale e file di base

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'anagrafiche.apps.AnagraficheConfig',
+    'competizioni.apps.CompetizioniConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
